c386e593278801cc4e4a6debfb50e7a17ed33a55
fcosuport <fcosuport@gmail.com>
2023-07-02 15:57:50 -0400

    token axios

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,5 +1,6 @@
 import { boot } from 'quasar/wrappers'
 import axios from 'axios'
+import { useAuthStore } from '../stores/auth'
 
 // Be careful when using SSR for cross-request state pollution
 // due to creating a Singleton instance here;
@@ -8,6 +9,17 @@ import axios from 'axios'
 // "export default () => {}" function below (which runs individually
 // for each client)
 const api = axios.create({ baseURL: 'http://localhost:5098/api/' })
+
+// Adicione o interceptor de requisições
+api.interceptors.request.use((config) => {
+  const token = useAuthStore().token // Obtenha o token do store
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}` // Adicione o token ao header "Authorization"
+  }
+
+  return config
+})
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
